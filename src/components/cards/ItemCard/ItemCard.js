d513e3536298c71daeb192d62811f1ada2058774
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-09-23 12:33:03 +0200

    working on Home Page

--- src/components/cards/ItemCard/ItemCard.js
+++ src/components/cards/ItemCard/ItemCard.js
@@ -2,19 +2,19 @@ import React from "react";
 
 const ItemCard = () => {
   return (
-    <div className="w-72 h-96 border-solid border-2 border-slate-200 relative rounded-lg">
+    <div className="w-72 h-96 border-solid border-2 border-orange-200 relative rounded-lg">
       <img
         src={require(`../../../images/logo.png`)}
         alt="item_image"
         className="w-full h-full absolute top-0 z-10 object-cover"
       />
-      <p className="absolute bottom-10 left-4 z-20 px-2 cursor-default bg-slate-400/[.4] rounded-lg">
+      <p className="absolute bottom-10 left-4 z-20 px-2 cursor-default text-slate-900 bg-orange-300/[.4] rounded-lg">
         Item_name
       </p>
-      <p className="absolute bottom-1 left-4 z-20 px-2 cursor-default bg-slate-400/[.4] rounded-lg">
+      <p className="absolute bottom-1 left-4 z-20 px-2 cursor-default text-slate-900 bg-orange-300/[.4] rounded-lg">
         Price
       </p>
-      <button className="absolute bottom-5 right-2 z-20 cursor-pointer bg-slate-400/[.5] p-1 rounded-lg">
+      <button className="absolute bottom-5 right-2 z-20 cursor-pointer text-slate-900 bg-orange-400/[.5] p-1 rounded-lg">
         ADD TO CART
       </button>
     </div>
--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-  return <h1 className="text-3xl font-bold underline">Pocetna stranica</h1>;
+  return (
+    <>
+      <h1 className="m-auto w-5/6 text-5xl font-bold  text-center text-slate-800 my-16 border-2 border-dashed border-orange-300 rounded-xl">
+        Welcome to our shopping cart application
+      </h1>
+      <p className="text-2xl font-bold  text-center text-slate-700 my-8">
+        Please
+        <Link
+          to="items"
+          className="text-3xl p-1 bg-orange-100 rounded-md text-cyan-700"
+        >
+          click me
+        </Link>
+        to see our products
+      </p>
+    </>
+  );
 };
 
 export default HomePage;
