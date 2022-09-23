557b081bb7e973c511cfcb23735be10ca2054500
dzenankosuta <dzenan_kosuta@hotmail.com>
2022-09-23 10:43:17 +0200

    adding opacity and border radius to item props

--- src/components/pages/CartPage/CartPage.js
+++ src/components/pages/CartPage/CartPage.js
@@ -7,13 +7,13 @@ const CartPage = () => {
         <div className="w-72 h-96 border-solid border-2 border-slate-200 relative rounded-lg">
           <img
             src={require(`../../../images/logo.png`)}
-            alt="cart_image"
+            alt="item_image"
             className="w-full h-full absolute top-0 z-10 object-cover"
           />
-          <p className="absolute bottom-10 left-4 z-20 px-2 bg-slate-800/[.06]">
+          <p className="absolute bottom-10 left-4 z-20 px-2 bg-slate-400/[.4] rounded-lg">
             Item_name
           </p>
-          <p className="absolute bottom-1 left-4 z-20 px-2 bg-slate-800/[.06]">
+          <p className="absolute bottom-1 left-4 z-20 px-2 bg-slate-400/[.4] rounded-lg">
             Price
           </p>
         </div>
